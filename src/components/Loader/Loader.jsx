11ccce4d40e8e4c51ee0273d25e2c87b573db08e
mariavsl518 <mariavsl518@gmail.com>
2024-01-05 12:36:24 +0100

    loader fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-// import css from './Loader.module.css'
+import css from './Loader.module.css'
 import { InfinitySpin } from 'react-loader-spinner'
 
 export const Loader = () => {
   return (
+    <div className={css.loaderBG}>
     <InfinitySpin
-    // className={css.loader}
     visible={true}
     width="600"
     color="#4E75B5"
     ariaLabel="infinity-spin-loading"
     />
+    </div>
         )
   
 }
